199d371808696f4f2c0f9f397d01d0ba420ee064
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-11-17 13:41:48 -0500

    WIP

--- timeout.js
+++ timeout.js
@@ -4,9 +4,15 @@
     var
         _TIMEOUT    = 0,
         _INTERVAL   = 1,
+        _CLEAR      = 2,
+        Func        = Function, // Bypass JSHint W054
 
+        _timeoutID = 0,
         _timeouts   = {},
-        _intervals  = {};
+        _intervalID = 0,
+        _intervals  = {},
+
+        _worker;
 
     function _timeout() {
         self.postMessage({
@@ -25,28 +31,76 @@
             definition = _intervals[id];
         }
         if (undefined !== definition) {
-            definition.callback.apply(window, definition.parameters);
+            definition[0].apply(window, definition.slice(1));
         }
     }
 
-    function _setTimeout() {
+    function _register(type, map, id, initialArguments) {
+        var delay = initialArguments[1] || 0,
+            callback = initialArguments[0];
+        if ("string" === typeof callback) {
+            callback = new Func(callback);
+        }
+        map[id] = [callback].concat(initialArguments.slice(2));
+        _worker.postMessage({
+            type: type,
+            delay: delay,
+            id: id
+        });
+        return id;
+    }
 
+    function _setTimeout() {
+        return _register(_TIMEOUT, _timeouts, ++_timeoutID, arguments);
     }
 
     function _clearTimeout(id) {
-
+        delete _timeouts[id];
+        _worker.postMessage({
+            type: _TIMEOUT + _CLEAR,
+            id: id
+        });
     }
 
     function _setInterval() {
-
+        return _register(_INTERVAL, _intervals, ++_intervalID, arguments);
     }
 
     function _clearInterval(id) {
-
+        delete _intervals[id];
+        _worker.postMessage({
+            type: _INTERVAL + _CLEAR,
+            id: id
+        });
     }
 
     function _workerMessage(event) {
-
+        var
+            type = event.data.type,
+            incomingID = event.data.id,
+            systemID,
+            map,
+            clearCmd,
+            setCmd;
+        if (_INTERVAL === (_INTERVAL & _INTERVAL)) {
+            map = _intervals;
+            clearCmd = clearInterval;
+            setCmd = setInterval;
+        } else {
+            map = _timeouts;
+            clearCmd = clearTimeout;
+            setCmd = setTimeout;
+        }
+        if (_CLEAR === (_CLEAR & type)) {
+            systemID = map[incomingID];
+            if (undefined !== systemID) {
+                clearCmd(systemID);
+                delete map[incomingID];
+            }
+        } else {
+            systemID = setCmd(_timeout, event.data.delay, [type, incomingID]);
+            map[incomingID] = systemID;
+        }
     }
 
     if ("undefined" !== typeof window) {
@@ -54,8 +108,8 @@
          * The Web Worker does not have access to the window, this is a good
          * condition to detect if we are in the page or in the thread.
          */
-        var worker = new Worker("timeout.js");
-        worker.addEventListener("message", _windowMessage);
+        _worker = new Worker("timeout.js");
+        _worker.addEventListener("message", _windowMessage);
         window.setTimeout = _setTimeout;
         window.clearTimeout = _clearTimeout;
         window.setInterval = _setInterval;
