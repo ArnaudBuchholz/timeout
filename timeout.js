19e5b526cba1b950e78563105ea733af984f647b
Arnaud Buchholz <arnaud.buchholz@free.fr>
2014-11-17 23:50:45 -0500

    First version (using URL to load the worker)

--- timeout.js
+++ timeout.js
@@ -14,19 +14,20 @@
 
         _worker;
 
-    function _timeout() {
-        self.postMessage({
-            type: arguments[0],
-            id: arguments[1]
-        });
-    }
-
+    /**
+     * Window worker.onmessage handler
+     *
+     * @param {{data: Object}} event
+     * @private
+     */
     function _windowMessage(event) {
         var type = event.data.type,
             id = event.data.id,
             definition;
         if (_TIMEOUT === type) {
             definition = _timeouts[id];
+            // Works even if definition is undefined
+            delete _timeouts[id];
         } else {
             definition = _intervals[id];
         }
@@ -35,6 +36,17 @@
         }
     }
 
+    /**
+     * Register the timeout/interval specification and sends a message to the
+     * worker thread to be notified back when necessary.
+     *
+     * @param {Number} type
+     * @param {Object} map
+     * @param {Number} id
+     * @param {Array} initialArguments
+     * @returns {Number}
+     * @private
+     */
     function _register(type, map, id, initialArguments) {
         var delay = initialArguments[1] || 0,
             callback = initialArguments[0];
@@ -50,10 +62,22 @@
         return id;
     }
 
+    /**
+     * setTimeout replacement
+     *
+     * @returns {Number} timeoutID
+     * @private
+     */
     function _setTimeout() {
         return _register(_TIMEOUT, _timeouts, ++_timeoutID, arguments);
     }
 
+    /**
+     * clearTimeout replacement
+     *
+     * @param {Number} id timeoutID
+     * @private
+     */
     function _clearTimeout(id) {
         delete _timeouts[id];
         _worker.postMessage({
@@ -62,10 +86,22 @@
         });
     }
 
+    /**
+     * _setInterval replacement
+     *
+     * @returns {Number} intervalID
+     * @private
+     */
     function _setInterval() {
         return _register(_INTERVAL, _intervals, ++_intervalID, arguments);
     }
 
+    /**
+     * _clearInterval replacement
+     *
+     * @param {Number} id intervalID
+     * @private
+     */
     function _clearInterval(id) {
         delete _intervals[id];
         _worker.postMessage({
@@ -74,6 +110,26 @@
         });
     }
 
+    /**
+     * Notify the main window that the timeout/interval occurred.
+     *
+     * @param {Number} type
+     * @param {Number} id
+     * @private
+     */
+    function _notify(type, id) {
+        self.postMessage({
+            type: type,
+            id: id
+        });
+    }
+
+    /**
+     * Worker worker.onmessage handler
+     *
+     * @param {{data: Object}} event
+     * @private
+     */
     function _workerMessage(event) {
         var
             type = event.data.type,
@@ -98,7 +154,7 @@
                 delete map[incomingID];
             }
         } else {
-            systemID = setCmd(_timeout, event.data.delay, [type, incomingID]);
+            systemID = setCmd(_notify, event.data.delay, [type, incomingID]);
             map[incomingID] = systemID;
         }
     }
